34f77943bad928c776fefce5794244531a211de1
unknown <manoj.pasupuletik@gmail.com>
2019-04-29 18:39:54 +0530

    notification local and firebase and splash screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,32 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { Firebase } from '@ionic-native/firebase/ngx';
+
+const config = {
+  apiKey: "AIzaSyAW9_c1Ox7_58GMaTtTLo6Bega68ooIjp4",
+  authDomain: "notification-500d1.firebaseapp.com",
+  databaseURL: "https://notification-500d1.firebaseio.com",
+  projectId: "notification-500d1",
+  storageBucket: "notification-500d1.appspot.com",
+  messagingSenderId: "923874329442"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule.initializeApp(config),
+    AngularFirestoreModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    Firebase,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    LocalNotifications
   ],
   bootstrap: [AppComponent]
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,77 @@
 import { Component } from '@angular/core';
+import { Platform, AlertController } from '@ionic/angular';
+import { LocalNotifications, ELocalNotificationTriggerUnit } from "@ionic-native/local-notifications/ngx";
+import { headersToString } from 'selenium-webdriver/http';
+import { scheduled } from 'rxjs';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+  scheduled = [];
+  constructor(private plt: Platform, private localnotifications: LocalNotifications, private alertcnt: AlertController) {
+    this.plt.ready().then(() => {
+      this.localnotifications.on('click').subscribe(res => {
+        let msg = res.data ? res.data.mydata : '';
+        this.showAlert(res.title, res.text, msg);
+      });
+      this.localnotifications.on('trigger').subscribe(res => {
+        let msg = res.data ? res.data.mydata : '';
+        this.showAlert(res.title, res.text, msg);
+      });
+    })
+  }
+  scheduleNotification() {
+    this.localnotifications.schedule
+      ({
+        id: 1,
+        title: 'Attention',
+        text: 'Local Notification',
+        data: { mydata: 'My data is here' },
+        trigger: { in: 5, unit: ELocalNotificationTriggerUnit.SECOND }
+
+      });
+  }
+  recurringNotification() {
+    this.localnotifications.schedule
+      ({
+        id: 2,
+        title: 'Recuring',
+        text: 'Local Notification',
+        data: { mydata: 'My data is here' },
+        trigger: { every: ELocalNotificationTriggerUnit.MINUTE }
+
+      });
+  }
+  repeatingDaily() {
+    this.localnotifications.schedule
+      ({
+        id: 4,
+        title: 'Good Morning',
+        text: 'Local Notification',
+        data: { mydata: 'My data is here' },
+        trigger: { every: { hour: 11, minute: 59 } }
+
+      });
+  }
+  getAll() {
+    this.localnotifications.getAll().then(res => {
+      this.scheduled = res;
+    })
+
+  }
+  showAlert(header, sub, msg) {
+    this.alertcnt.create({
+      header: header,
+      subHeader: sub,
+      message: msg,
+      buttons: ['ok']
+
+    }).then(alert => alert.present());
+
+  }
+}
